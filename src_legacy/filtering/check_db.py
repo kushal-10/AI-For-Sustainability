3ab9e3b70ef99dd777db05d4b3a0c63404a58398
kushal-10 <kushalkoshti11@gmail.com>
2026-03-30 03:40:34 +0200

    rm legacy code

--- src_legacy/filtering/check_db.py
+++
@@ -1,8 +0,0 @@
-import duckdb
-
-con = duckdb.connect("data/dbs/sdg_hits.duckdb")
-df = con.execute("SELECT * FROM sdg_hits").fetchdf()
-
-print(len(df))
-print(df.tail())   # prints in a table-like format
-print(df.head())
